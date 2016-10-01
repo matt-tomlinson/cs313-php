e361a2ce2e4379e2b0adc287dc156e1f26291897
Matt <matt.tomlinson1@gmail.com>
2016-10-01 08:15:37 -0600

    i broke the form

--- web/resultsForm.php
+++ web/resultsForm.php
@@ -1,22 +1,25 @@
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="stylesheets/home.css">
 </head>
-
 <body>
     <h2 class="headerText">CS 313 - Web Engineering II</h2> 
     <h1 class="headerText">Student Survey</h1>        
     <div class="answerBox">
         <?php
-        $fileName = "data/results.txt";
+        $fileName = "results.txt";
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        function test_input($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+        }
+          if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $day = test_input($_POST["day"]);
             $season = test_input($_POST["season"]);
 
@@ -36,18 +39,11 @@
         
         else { echo "Please submit the form."; }
 
-        function test_input($data) {
-          $data = trim($data);
-          $data = stripslashes($data);
-          $data = htmlspecialchars($data);
-          return $data;
-      }
-
-      echo "Sunday: "; echo $results[0];
-      echo "<br/>";
-      echo "Monday: "; echo $results[1];
-      ?>
-      <?php echo $_POST["day"]; ?>
-  </div>
+    echo "Sunday: "; echo $results[0];
+    echo "<br/>";
+    echo "Monday: "; echo $results[1];
+    ?>
+    <?php echo $_POST["day"]; ?>
+</div>
 </body>
 </html>
