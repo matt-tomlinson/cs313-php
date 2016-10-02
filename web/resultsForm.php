eeacd584c6dac92cb1856ceb1842bd9fa2525c55
Matt <matt.tomlinson1@gmail.com>
2016-10-01 18:20:13 -0600

    session testing8

--- web/resultsForm.php
+++ web/resultsForm.php
@@ -2,11 +2,11 @@
 
 $file = fopen("results.txt", "a") or die("Unable to open file!");
 
-$answer = $_POST['day'] .= "  "
-.= $_POST['season'] .= "  "
-.= $_POST['meal'] .= "  "
-.= $_POST['pet'] .= "  "
-.= "<br/>";
+$answer  = $_POST['day'] .= "  ";
+$answer .= $_POST['season'] .= "  ";
+$answer .= $_POST['meal'] .= "  ";
+$answer .= $_POST['pet'] .= "  ";
+$answer .= "<br/>";
 
 fwrite($file, $answer);
 fclose($file);
