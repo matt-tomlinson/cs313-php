d108d493197a46196ee50b4ae5aea10898431f1d
Matt <matt.tomlinson1@gmail.com>
2016-10-13 17:32:50 -0600

    added scripture assignment files from tutorial

--- web/groupWork/scriptures.php
+++ web/groupWork/scriptures.php
@@ -13,7 +13,7 @@ if (empty($dbUrl)) {
 
 $dbopts = parse_url($dbUrl);
 
-print "<p>$dbUrl</p>\n\n";
+print "<p class="answerBox">$dbUrl</p>\n\n";
 
 $dbHost = $dbopts["host"]; 
 $dbPort = $dbopts["port"]; 
@@ -21,22 +21,43 @@ $dbUser = $dbopts["user"];
 $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
 
-print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+print "<p class="answerBox">pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
 
 try {
  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 }
 catch (PDOException $ex) {
- print "<p>error: $ex->getMessage() </p>\n\n";
+ print "<p class="answerBox">error: $ex->getMessage() </p>\n\n";
  die();
 }
 
 foreach ($db->query('SELECT now()') as $row)
 {
- print "<p>$row[0]</p>\n\n";
+ print "<p class="answerBox">$row[0]</p>\n\n";
 }
 
 ?>
+
+<?php
+$title = 'Week 5 Prep';
+$description = "Week 5 Prep for CS 313.";
+include $_SERVER['DOCUMENT_ROOT'] . '/cs313/views/addins/header.php'; 
+?>
+<h1 class="answerBox">Week 5: Team Readiness - Scriptures</h1>
+<main class="answerBox">
+    <br>
+    <hr>
+    <h2 class="answerBox">SCRIPTURE RESOURCES</h2>
+    <?php 
+    foreach($stmt->fetchAll() as $row){
+        echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b>';
+        echo ' - "' . $row['content'] . '"';
+        echo '<br/><br/>';
+    }
+    ?>
+    <hr>
+</main>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/cs313/views/addins/footer.php'; ?>
 
 </body>
 </html>
