419595e6d160fa89ba916b540d51d4fc8b9e6957
Matt <matt.tomlinson1@gmail.com>
2016-10-18 19:53:42 -0600

    test insert

--- web/games2.php
+++ web/games2.php
@@ -30,15 +30,15 @@
 			</tr>
 			
 			<?php
-		// default Heroku Postgres configuration URL
+			// default Heroku Postgres configuration URL
 			$dbUrl = getenv('DATABASE_URL');
 			if (empty($dbUrl)) {
-		// example localhost configuration URL with postgres username and a database called cs313db
+			// example localhost configuration URL with postgres username and a database called cs313db
 				$dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
 			}
 
 			$dbopts = parse_url($dbUrl);
-		//print "<p>$dbUrl</p>\n\n";
+			//print "<p>$dbUrl</p>\n\n";
 			$dbHost = $dbopts["host"]; 
 			$dbPort = $dbopts["port"]; 
 			$dbUser = $dbopts["user"]; 
