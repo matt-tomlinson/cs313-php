32b0cd7308986b649374e36ae0406e129d74b497
Matt <matt.tomlinson1@gmail.com>
2016-10-25 18:08:35 -0600

    session test1

--- web/loggedin.php
+++ web/loggedin.php
@@ -6,12 +6,61 @@
     <link rel="stylesheet" type="text/css" href="stylesheets/home.css">
 </head>
 <body>
-    <h1 class="headerText">Student Survey Results</h1>        
+    <h1 class="headerText">Login Page</h1>        
     <div class="answerBox">
-        <?php 
-        $username = $_POST['username'];
+        <?php
+
+        if (isset($_SESSION['loggedin'])) {
+            header("Location: loggedin.php");
+            exit;
+        }
+
+        $dbUrl = getenv('DATABASE_URL');
+        
+        if (empty($dbUrl)) {
+            $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+        }
+        $dbopts = parse_url($dbUrl);
+        $dbHost = $dbopts["host"];
+        $dbPort = $dbopts["port"];
+        $dbUser = $dbopts["user"];
+        $dbPassword = $dbopts["pass"];
+        $dbName = ltrim($dbopts["path"],'/');
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            try {
+                $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                $q = "SELECT * FROM users WHERE username='".$username."'";
+                foreach ($db->query($q) as $row) {
+
+                    if (password_verify($password, $hash)) {
+                        //$message = "Invalid Credentials";
+                        //echo "<script type='text/javascript'>alert('$message');</script>";
+                        //echo "<span style='color:red'>Invalid Credentials</span>";
+                    }
+                    else {
+                        $_SESSION["loggedin"] = $username;
+                        header("Location: loggedin.php");
+                        exit;
+
+                    }
+                }
+            }
+            catch (PDOException $ex) {
+                print "<p>error: $ex->getMessage() </p>\n\n";
+                die();
+            }
+        }
+
         echo '<p>Welcome '. $username. '!</p>';
         echo '<p>Welcome '. $_SESSION['loggedin'] .'!</p>';
+
         ?>
     </div>
 </body>
--- web/login.php
+++ web/login.php
@@ -6,7 +6,8 @@
 	<script>
 		function check_empty() {
 			if (document.getElementById('username').value == "" || document.getElementById('password').value == "" ) {
-				alert("Please fill out all fields.");
+				//alert("Please fill out all fields.");
+				document.getElementById('error').value == "Invalid Credentials";
 				return false;
 			} else {
 				document.getElementById('form').submit();
@@ -18,13 +19,13 @@
 	<div class="loginbox">
 		<h1 class="headerText">Login</h1>
 		<div>
-			<form onsubmit="return check_empty()" action="login.php" id="form" method="post" name="form">
+			<form onsubmit="return check_empty()" action="loggedin.php" id="form" method="post" name="form">
 				<input class="login" id="username" name="username" placeholder="Username" type="text">
 				<input class="login" id="password" name="password" placeholder="Password" type="password">
 				<input class="login" type="submit" name="login" value="Login" />
 			</form>
 			<a href="signup.php" target="targetframe">Sign-up</a>
-			<span id="error"></span>
+			<span id="error" style="color:red;"></span>
 		</div>
 	</div>
 	<hr>
@@ -32,53 +33,6 @@
 
 	session_start();
 
-	if (isset($_SESSION['loggedin'])) {
-		header("Location: loggedin.php");
-		exit;
-	}
-	// default Heroku Postgres configuration URL
-	$dbUrl = getenv('DATABASE_URL');
-	if (empty($dbUrl)) {
-	// example localhost configuration URL with postgres username and a database called cs313db
-		$dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
-	}
-	$dbopts = parse_url($dbUrl);
-	$dbHost = $dbopts["host"];
-	$dbPort = $dbopts["port"];
-	$dbUser = $dbopts["user"];
-	$dbPassword = $dbopts["pass"];
-	$dbName = ltrim($dbopts["path"],'/');
-
-	if ($_SERVER['REQUEST_METHOD'] == "POST") {
-		try {
-			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-
-			$hash = password_hash($password, PASSWORD_DEFAULT);
-
-			$q = "SELECT * FROM users WHERE username='".$username."'";
-			foreach ($db->query($q) as $row) {
-
-				if (password_verify($password, $hash)) {
-					$_SESSION["loggedin"] = $username;
-					header("Location: loggedin.php");
-					exit;
-				}
-				else {
-    				//$message = "Invalid Credentials";
-					//echo "<script type='text/javascript'>alert('$message');</script>";
-					//echo "<span style='color:red'>Invalid Credentials</span>";
-				}
-			}
-		}
-		catch (PDOException $ex) {
-			print "<p>error: $ex->getMessage() </p>\n\n";
-			die();
-		}
-	}
-	
 	?>
 </body>
 </html>
